b64d60a8adb4ae13ee43b67dd859ff9470717bf4
TotalyNotAlex <totalynotalex@users.noreply.github.com>
2026-07-24 18:50:44 +0200

    bump v1.1.0 -> v1.2.0 (Skin Changer + Bugfixes)

--- services/updateService.js
+++ services/updateService.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
 
-const CURRENT_VERSION = '1.1.0';
+const CURRENT_VERSION = '1.2.0';
 let manifestPath = '';
 
 class UpdateService {
