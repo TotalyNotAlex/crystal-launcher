1082cffa2fc0ae543ea8baf744f0a5586b3fa1aa
TotalyNotAlex <totalynotalex@users.noreply.github.com>
2026-07-27 16:44:13 +0200

    Bump to 1.4.4 - fix overlay jar install path + bundle via extraResources

--- services/updateService.js
+++ services/updateService.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
 
-const CURRENT_VERSION = '1.4.3';
+const CURRENT_VERSION = '1.4.4';
 let manifestPath = '';
 
 class UpdateService {
