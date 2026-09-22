41bc781bc718ff878b8cdb0bccc7c59cddb3af4f
TotalyNotAlex <totalynotalex@users.noreply.github.com>
2026-09-23 01:32:22 +0200

    Bump to 1.4.14 - trigger update popup

--- main.js
+++ main.js
@@ -378,7 +378,7 @@ function getPlaytimeSessions() {
 }
 
 ipcMain.handle('check-updates', async () => updateService.checkForUpdates());
-ipcMain.handle('get-app-version', async () => { try { return app.getVersion(); } catch { return '1.4.6'; } });
+ipcMain.handle('get-app-version', async () => { try { return app.getVersion(); } catch { return '1.4.14'; } });
 
 ipcMain.handle('get-saved-skins', async () => {
   const skinDir = path.join(baseDataDir, 'skins');
